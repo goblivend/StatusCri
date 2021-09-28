fa41d48b0ec915c319936efd24ab4ea3f7e1bfb0
Ivan Imbert <ivan.imbert@epita.fr>
2021-09-28 08:33:53 +0200

    Embed now preset

--- Commands/help.js
+++ Commands/help.js
@@ -1,19 +1,14 @@
-//const Embed = require('../Embeds');
-
-import MyEmbed from 'Embeds.js';
-
+const Embed = require('../Embeds');
 
 
 module.exports = {
     name: 'help',
     description: 'Send an helping message',
     execute(message, args, Discord){
-        test = MyEmbed();
-        /*const Embed = Discord.MessageEmbed()
+        const embed = Embed.MyEmbed(new Discord.MessageEmbed)
             .setColor('#0099ff')
             .setTitle('Help guide')
             //.setURL('')
-            .setAuthor('Ivan (goblivend)', 'https://cdn.discordapp.com/avatars/234242303273861130/677409428206ef9aada9517e0a70b1be.png?size=32', 'https://github.com/goblivend/StatusCri')
             .setDescription('An help message ')
             .setThumbnail('https://i.imgur.com/AfFp7pu.png')
             .addFields(
@@ -23,10 +18,8 @@ module.exports = {
                 { name: 'Inline field title', value: 'Some value here', inline: true },
             )
             .addField('Inline field title', 'Some value here', true)
-            .setImage('https://i.imgur.com/AfFp7pu.png')
-            .setTimestamp()
-            .setFooter('Some footer text here', 'https://cdn.discordapp.com/avatars/234242303273861130/677409428206ef9aada9517e0a70b1be.png?size=32');
-*/
+            .setImage('https://i.imgur.com/AfFp7pu.png');
+        message.channel.send({embeds : [embed]});
 
     }
 
--- Embeds.js
+++ Embeds.js
@@ -1,3 +1,10 @@
-function MyEmbed(){
-    
+module.exports = {
+    MyEmbed : function(Embed) {
+        let Picture = 'https://cdn.discordapp.com/avatars/234242303273861130/677409428206ef9aada9517e0a70b1be.png?size=32';
+        Embed
+            .setAuthor('Ivan (goblivend)', Picture, 'https://github.com/goblivend/StatusCri')
+            .setTimestamp()
+            .setFooter('Some footer text here', Picture);
+        return Embed;
+    }
 }
